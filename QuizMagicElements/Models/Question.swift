8c805da2e0edaeac30ffed78d4b9b7aa8e246b49
Ruslan <ruslan.shtybaev@gmail.com>
2022-03-05 12:13:50 +0600

    end model

--- QuizMagicElements/Models/Question.swift
+++ QuizMagicElements/Models/Question.swift
@@ -7,8 +7,44 @@
 
 struct Question {
     let title: String
-    let answers: [Answer]
     let type: ResponceType
+    let answers: [Answer]
+    
+    static func getQuestions() -> [Question] {
+        [
+            Question(
+                title: "Какую пищу вы предпочитаете?",
+                type: .single,
+                answers: [
+                    Answer(title: "Стейк", animal: .dog),
+                    Answer(title: "Рыба", animal: .cat),
+                    Answer(title: "Морковь", animal: .rabbit),
+                    Answer(title: "Черепаха", animal: .turtle)
+                    ]
+                
+            ),
+            Question(
+                title: "Что вам нравится больше?",
+                type: .miltiple,
+                answers: [
+                    Answer(title: "Плавать", animal: .dog),
+                    Answer(title: "Спать", animal: .cat),
+                    Answer(title: "Обниматься", animal: .rabbit),
+                    Answer(title: "Есть", animal: .turtle)
+                ]
+            ),
+            Question(
+                title: "Любите ли вы ездить на машине?",
+                type: .ranges,
+                answers: [
+                    Answer(title: "Ненавижу", animal: .cat),
+                    Answer(title: "Нервничаю", animal: .turtle),
+                    Answer(title: "Не замечаю", animal: .rabbit),
+                    Answer(title: "Нервничаю", animal: .dog)
+                ]
+            )
+        ]
+    }
 }
 
 struct Answer {
